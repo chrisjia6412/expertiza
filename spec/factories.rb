835c65b178b39fdf5124181c7f9a3ce817f265ed
yhong3 <yhong3@ncsu.edu>
2014-03-20 01:16:13 -0400

    Just stopped here because the missing fixtures just arrived!!!!

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,52 @@
 FactoryGirl.define do
   factory :user do
-    first_name "John"
-    last_name  "Doe"
-    admin false
+    sequence(:name)  { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@user.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    trait :student do
+      role_id 1
+    end
+
+    trait :instructor do
+      role_id 2
+    end
+
+    trait :admin do
+      role_id 4
+    end
+
+    trait :ta do
+      role_id 5
+    end
   end
 
-  # This will use the User class (Admin would have been guessed)
-  factory :admin, class: User do
-    first_name "Admin"
-    last_name  "User"
-    admin      true
+  factory :course do
+    name "class"
+    association :instructor, factory: [:user, :instructor]
+    directory_path "class"
   end
+
+  factory :assignment do
+    name "ass"
+    directory_path "ass"
+    association :course, factory: [:course]
+    association :instructor, factory: [:user, :instructor]
+    require_signup true
+    max_team_size 3
+  end
+
+  factory :topic do
+    association :assignment, factory: :assignment
+    sequence(:topic_name)  { |n| "topic#{n}" }
+    sequence(:topic_identifier) { |n| "topic#{n}"}
+  end
+
+  factory :participants do
+    association :user, factory: [:user, :student]
+    association :assignment, factory: :assignment
+    type "AssignmentParticipant"
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,17 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 #
+#require 'authlogic/test_case'
+require 'factory_girl'
+#include Authlogic::TestCase
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.include FactoryGirl::Syntax::Methods
+
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
